951b69174e549a39b480e619c418f160b796f0a6
rouzbeh-rahbar <rouzbeh.rahbar4@gmail.com>
2020-12-07 20:38:53 +0100

    first commit

---
+++ main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void) 
+{
+  int i;
+  printf("Dieses Programm gibt die Zahlen von 14 bis 23 in aufsteigender Reihenfolge aus\n\n");
+  i=14;
+  while(i<=23)
+  {
+    printf("%d\n",i);
+    i++;
+  }
+  return 0;
+}
